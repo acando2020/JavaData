3afdebc77bd0c2d33f45514c6a15228fc4b3230c
acando2020 <javier.master1994@gmail.com>
2022-08-19 14:41:40 -0500

    Update RBook.java

--- src/main/java/com/example/DataGrid/Repository/RBook.java
+++ src/main/java/com/example/DataGrid/Repository/RBook.java
@@ -8,10 +8,6 @@ import com.example.DataGrid.Model.Libro;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author jonathan
- */
 public interface RBook extends JpaRepository<Libro, Long>{
     List<Libro> findByNamebookContaining(String namebook);
 }
